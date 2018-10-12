3792daedee44d4d58dd875b3cfa4eeb38297644d
okdalto <ippx123@gmail.com>
2018-10-12 14:31:59 +0900

    김성현 개인코드 추가했습니다.
    김성현 개인코드 추가했습니다.
    
    주석 추후 달겠습니다..

---
+++ 개별코드/kimSeonghyeon/ArcBallUI.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArcBallUI : MonoBehaviour {
+    public Camera ObjectCamera;
+    public GameObject targetGo;
+    private Vector3 mousePosition;
+    private Vector3 pMousePosition;
+    public float scrollScale;
+    // Use this for initialization
+    void Start () {
+        pMousePosition = Vector3.zero;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            pMousePosition = Input.mousePosition;
+        }
+        else if (Input.GetMouseButton(0))
+        {
+            mousePosition = Input.mousePosition;
+            Vector3 left = (pMousePosition - mousePosition);
+            Vector3 crossedVector = Vector3.Cross(Vector3.Normalize(left), ObjectCamera.transform.rotation * Vector3.forward);
+            pMousePosition = Input.mousePosition;
+            targetGo.transform.RotateAround(targetGo.transform.position, - crossedVector, Vector3.Magnitude(left) * scrollScale);
+        }
+    }
+
+    public void setTargetGameObject(GameObject go)
+    {
+        Debug.Log("dsdssddsds");
+        Destroy(targetGo.transform.GetChild(0).gameObject);
+        GameObject tempGO = Instantiate(go, targetGo.transform);
+        tempGO.transform.localPosition = Vector3.zero;
+    }
+}
---
+++ 개별코드/kimSeonghyeon/RayCastFromMouse.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RayCastFromMouse : MonoBehaviour {
+    public float pokeForce;
+    public Text text;
+    public ArcBallUI ui;
+    // Use this for initialization
+    void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            //var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            var ray = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
+            Debug.DrawRay(ray.origin, ray.direction);
+            if (Physics.Raycast(ray, out hit))
+            {
+                Debug.Log(hit.transform.gameObject);
+                if (hit.transform.gameObject != null && hit.transform.gameObject.tag == "Selectable")
+                {
+                    ui.setTargetGameObject(hit.transform.gameObject);
+                }
+                text.text = hit.transform.gameObject.name;
+                if (hit.rigidbody != null)
+                {
+                    Debug.Log("dwdads");
+                    hit.rigidbody.AddForceAtPosition(ray.direction * pokeForce, hit.point);
+                }
+            }
+        }
+    }
+}
